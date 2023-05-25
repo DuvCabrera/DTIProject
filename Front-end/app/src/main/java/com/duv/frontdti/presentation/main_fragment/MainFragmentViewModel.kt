6f6bfa4f66b7f2151ca6f741cd357c152df2fcd5
Duv <eduardoviniciuscabrera@gmail.com>
2023-05-25 18:25:17 -0300

    Criado grafico de navegação

--- Front-end/app/src/main/java/com/duv/frontdti/presentation/main_fragment/MainFragmentViewModel.kt
+++ Front-end/app/src/main/java/com/duv/frontdti/presentation/main_fragment/MainFragmentViewModel.kt
@@ -1,8 +1,12 @@
 package com.duv.frontdti.presentation.main_fragment
 
 import androidx.lifecycle.ViewModel
+import com.duv.frontdti.domain.usecases.ReminderUC
 import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
 @HiltViewModel
-class MainFragmentViewModel : ViewModel() {
+class MainFragmentViewModel @Inject constructor(
+    private val reminderUC: ReminderUC
+) : ViewModel() {
 }
